def6421ef3b5e49b45f80e23e9c08ce3ad7e10a2
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-09-17 21:34:13 -0400

    separated base64 converter to plant.js, now using module.exports to run api calls with js pages

--- apis/plantid-request.js
+++ apis/plantid-request.js
@@ -1,14 +1,5 @@
 "use strict";
 var axios = require("axios");
-var fs = require("fs");
-
-// First grab image from plant.handlebars field
-var image = $("plant.handlebars").getElementById(imageToBase64);
-
-// Then convert image into base64
-var buff = fs.readFileSync(image);
-var base64data = buff.toString("base64");
-console.log("Image converted to base 64 is:\n\n" + base64data);
 
 // Send API POST request to Plant.ID to identify what's in the base64
 module.exports = {
