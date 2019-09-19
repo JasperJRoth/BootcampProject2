781b88e7f733ec3b0857ce25c5f27f26ed3e1c51
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-09-19 16:23:23 -0400

    moved image upload tool directly onto plant.handlebars

--- apis/plantid.js
+++ apis/plantid.js
@@ -1,15 +1,15 @@
-"use strict";
+// "use strict";
 
 var axios = require("axios");
-var fs = require("fs");
+// var fs = require("fs");
 
 // First grab image from plant.handlebars field
-// var image = $("plant.handlebars").getElementById(imageToBase64);
+// var image = $("../views/plant.handlebars").getElementById(imageToBase64);
 
 // Then convert image into base64
-var buff = fs.readFileSync("../images/garlic_mustard_flowering.jpg"); // CHANGE THIS TO THE IMAGE VAR BEFORE PRESENTING
-var base64data = buff.toString("base64");
-console.log("Image converted to base 64 is:\n\n" + base64data);
+// var buff = fs.readFileSync(image); // CHANGE THIS TO THE IMAGE VAR BEFORE PRESENTING
+// var base64data = buff.toString("base64");
+// console.log("Image converted to base 64 is:\n\n" + base64data);
 
 // Send API POST request to Plant.ID to identify what's in the base64
 module.exports = {
--- public/js/plant.js
+++ public/js/plant.js
@@ -1,3 +1,11 @@
+"use strict";
+
+var fs = require("fs");
 var plantIDRequest = require("../../apis/plantid");
 
-plantIDRequest.sendPlantIDRequest();
+var buff = fs.readFileSync(image); // CHANGE THIS TO THE IMAGE VAR BEFORE PRESENTING
+var base64data = buff.toString("base64");
+console.log("Image converted to base 64 is:\n\n" + base64data);
+
+plantIDRequest.sendPlantIDRequest(imgTest);
+alert(imgTest);
