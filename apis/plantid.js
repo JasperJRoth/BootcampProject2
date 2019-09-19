52351d135ad46a9602355841ae8a2d0527090237
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-09-19 17:39:09 -0400

    plantid almost completely works on browser - leaving node framework commented out just in case

--- apis/plantid.js
+++ apis/plantid.js
@@ -1,15 +1,15 @@
 // "use strict";
 
 var axios = require("axios");
-// var fs = require("fs");
+var fs = require("fs");
 
 // First grab image from plant.handlebars field
-// var image = $("../views/plant.handlebars").getElementById(imageToBase64);
+var image = ("../../public/images/garlic_mustard_flowering.jpg");
 
-// Then convert image into base64
-// var buff = fs.readFileSync(image); // CHANGE THIS TO THE IMAGE VAR BEFORE PRESENTING
-// var base64data = buff.toString("base64");
-// console.log("Image converted to base 64 is:\n\n" + base64data);
+// // Then convert image into base64
+var buff = fs.readFileSync(image);
+var base64data = buff.toString("base64");
+console.log("Image converted to base 64 is:\n\n" + base64data);
 
 // Send API POST request to Plant.ID to identify what's in the base64
 module.exports = {
@@ -18,13 +18,16 @@ module.exports = {
       axios.post("https://api.plant.id/identify", {
         key: "waFqlu35RJTHeWYESGUTx6uIvPKvet4VmWgSx7TTu3xt0RDYKt", // Need to hide the key first
         images: [base64data],
+        // eslint-disable-next-line camelcase
         custom_id: 420,
         headers: {
           "Content-Type": "application/json"
         }
+      // eslint-disable-next-line no-unused-vars
       }).then(function(response){
         return axios.post("https://api.plant.id/check_identifications", {
           key: "waFqlu35RJTHeWYESGUTx6uIvPKvet4VmWgSx7TTu3xt0RDYKt",  
+          // eslint-disable-next-line camelcase
           custom_ids: [420],
           headers: {
             "Content-Type": "application/json"
--- public/js/plant.js
+++ public/js/plant.js
@@ -1,11 +1,11 @@
-"use strict";
+// "use strict";
 
-var fs = require("fs");
+// var fs = require("fs");
 var plantIDRequest = require("../../apis/plantid");
 
-var buff = fs.readFileSync(image); // CHANGE THIS TO THE IMAGE VAR BEFORE PRESENTING
-var base64data = buff.toString("base64");
-console.log("Image converted to base 64 is:\n\n" + base64data);
+// var buff = fs.readFileSync(image); // CHANGE THIS TO THE IMAGE VAR BEFORE PRESENTING
+// var base64data = buff.toString("base64");
+// console.log("Image converted to base 64 is:\n\n" + base64data);
 
-plantIDRequest.sendPlantIDRequest(imgTest);
-alert(imgTest);
+plantIDRequest.sendPlantIDRequest("imgTest");
+// console.log(response.data[1].suggestions[0].plant.common_name);
